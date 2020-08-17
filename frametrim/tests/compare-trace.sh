73e7175d0c0e7cfd9a3af79c105daad16739d37d
Gert Wollny <gert.wollny@collabora.com>
2020-08-17 13:21:50 +0200

    frametrim: Add second test

--- frametrim/tests/compare-trace.sh
+++ frametrim/tests/compare-trace.sh
@@ -14,8 +14,11 @@ ${trim} "${datadir}/${trace}" --frames ${frame} -o trim-${trace}
 ${apitrace} replay "${datadir}/${trace}" --snapshot=${orig_img} --snapshot-prefix=orig 
 ${apitrace} replay trim-${trace} --snapshot=${trim_img} --snapshot-prefix=trim 
 
-md5sum orig000000${orig_img}.png | sed -s "s/ .*//" >${trace}.orig.md5
-md5sum trim000000${trim_img}.png | sed -s "s/ .*//" >${trace}.trim.md5
+orig=$(printf "orig%010d.png" ${orig_img})
+trim=$(printf "trim%010d.png" ${trim_img})
+
+md5sum ${orig} | sed -s "s/ .*//" >${trace}.orig.md5
+md5sum ${trim} | sed -s "s/ .*//" >${trace}.trim.md5
 
 diff -q ${trace}.orig.md5 ${trace}.trim.md5 && exit 0 || exit 1
 
