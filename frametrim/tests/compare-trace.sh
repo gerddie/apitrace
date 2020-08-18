93e8bede7107e8ceefdcc075dd9a31cb2bcbe794
Gert Wollny <gert.wollny@collabora.com>
2020-08-18 15:08:20 +0200

    frametrim/tests: delete images before testing

--- frametrim/tests/compare-trace.sh
+++ frametrim/tests/compare-trace.sh
@@ -10,6 +10,8 @@ orig_img=$5
 
 echo "Run tests $trace" 
 
+rm trace0*.png
+
 ${trim} "${datadir}/${trace}" --frames ${frame} -o trim-${trace}
 ${apitrace} replay --headless "${datadir}/${trace}" --snapshot=${orig_img} --snapshot-prefix=orig || exit 1
 ${apitrace} replay --headless trim-${trace} --snapshot=frame --snapshot-prefix=${trace} || exit 1
