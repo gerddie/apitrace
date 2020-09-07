da28e7453d4cfa601c4c488ede50268a08eefccc
Gert Wollny <gert.wollny@collabora.com>
2020-09-07 12:37:39 +0200

    frametrim_reverse: tie in test for reverse frametrim

--- frametrim/tests/compare-trace_reverse.sh
+++ frametrim/tests/compare-trace_reverse.sh
@@ -1,7 +1,6 @@
-
 #/bin/sh
 
-trim="$1/glxtrim"
+trim="$1/glxtrimreverse"
 apitrace="$1/apitrace"
 datadir="$2"
 trace="$3"
@@ -14,15 +13,14 @@ rm ${trace}*.png
 rm ${trace}*.md5
 rm trim$-{trace}
 
-${trim} "${datadir}/${trace}" --frames ${frame} -o trim-${trace} || exit 1
+${trim} "${datadir}/${trace}" --frames ${frame} -o rtrim-${trace} || exit 1
 ${apitrace} replay --headless "${datadir}/${trace}" --snapshot=${orig_img} --snapshot-prefix=${trace}orig || exit 1
-${apitrace} replay --headless trim-${trace} --snapshot=frame --snapshot-prefix=${trace} || exit 1
+${apitrace} replay --headless rtrim-${trace} --snapshot=frame --snapshot-prefix=r${trace} || exit 1
 
 orig=$(printf "${trace}orig%010d.png" ${orig_img})
-trim=$(ls ${trace}0*.png)
+trim=$(ls r${trace}0*.png)
 
 md5sum ${orig} | sed -s "s/ .*//" >${trace}.orig.md5
-md5sum ${trim} | sed -s "s/ .*//" >${trace}.trim.md5
+md5sum ${trim} | sed -s "s/ .*//" >${trace}.rtrim.md5
 
-diff -q ${trace}.orig.md5 ${trace}.trim.md5 && exit 0 || exit 1
-
+diff -q ${trace}.orig.md5 ${trace}.rtrim.md5 && exit 0 || exit 1
