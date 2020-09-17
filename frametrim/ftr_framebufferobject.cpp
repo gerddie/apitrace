50ef0313b5d4887693e49cf4c862d3bfcfb6e1dc
Gert Wollny <gert.wollny@collabora.com>
2020-09-17 12:18:56 +0200

    frametrim_reverse: correct merge error

--- frametrim/ftr_framebufferobject.cpp
+++ frametrim/ftr_framebufferobject.cpp
@@ -139,7 +139,8 @@ PTraceCall FramebufferObject::clear(const trace::Call& call)
     return c;
 }
 
-void FramebufferObject::last_full_redraw_before(unsigned call_no) const
+unsigned
+FramebufferObject::last_full_redraw_before(unsigned call_no) const
 {
     for(auto c : m_full_clears) {
         if (c < call_no)
